121cf8e2318bab62229f6270b18bfeffb8c63e03
Nichjc123 <nichjc123@gmail.com>
2021-06-05 20:28:55 -0400

    Added button on header

--- Angular/task-tracker/src/app/components/button/button.component.ts
+++ Angular/task-tracker/src/app/components/button/button.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, Event Emitter } from '@angular/core';
+
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
-  styleUrls: ['./button.component.css']
+  styleUrls: ['./button.component.css'],
 })
 export class ButtonComponent implements OnInit {
+  @Input() text: string | undefined;
+  @Input() color: string | undefined;
+  @Output btnClick = new EventEmitter
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onClick() {
+    console.log('Add');
   }
-
 }
