62d81ae9717a12f092e917ecbc15c2f5ff6e9b9b
Nichjc123 <nichjc123@gmail.com>
2021-06-16 09:59:58 -0400

    Deleting functionality

--- Angular/task-tracker/src/app/components/tasks/tasks.component.ts
+++ Angular/task-tracker/src/app/components/tasks/tasks.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TaskService } from '../../services/task.service';
 import { Task } from '../../Task';
+import { TASKS } from 'src/app/mock-tasks';
 
 @Component({
   selector: 'app-tasks',
@@ -14,5 +15,13 @@ export class TasksComponent implements OnInit {
 
   ngOnInit(): void {
     this.taskService.getTasks().subscribe((tasks) => (this.tasks = tasks));
+  }
+
+  deleteTask(task: Task) {
+    this.taskService
+      .deleteTask(task)
+      .subscribe(
+        () => (this.tasks = this.tasks.filter((t) => t.id !== task.id))
+      );
   }
 }
